bc6afe55295fbf1374a1aa7b6ab003ff338831d9
Dio Pratama <dio.yoga@aksaramaya.com>
2022-01-11 14:10:41 +0700

    test heroku

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ class ContohResource(Resource):
         umur = request.form["umur"]
         identitas["nama"] = nama
         identitas["umur"] = umur
-        response = {"message": "Success"}
+        response = {"message": "Success poll"}
         return response
 
 #setup resource
